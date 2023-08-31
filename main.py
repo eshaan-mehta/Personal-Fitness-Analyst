4d57163d81e1dc6c3f6d096ab65357f5075290fa
eshaan-mehta <eshaanm.mehta@gmail.com>
2023-08-30 20:20:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ from setup import *
 
 prev_time = time.time()  #initial time calculation
 
+
+def overlay(frame):
+    #adding FPS overlay onto video
+    frame = cv.putText(img=frame,
+                    text="FPS: " + str(fps),
+                    org=(7, 30),
+                    fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1, 
+                    color=(100, 255, 0),
+                    thickness=2,
+                    )
+
+    
+
+
+
 while(True):
     #delta_time calculation to determine time per frame
     cur_time = time.time()
@@ -10,19 +26,12 @@ while(True):
     prev_time = cur_time
     fps = int(1/delta_time)
 
-    ret, frame = capture.read() #extracts single fram from video, ret is failure to receive video
+    ret, frame = capture.read() #extracts single fram from video, ret shows if receiving video
 
-    #flip image horizontally and overlay frame rate 
-    frame = cv.flip(frame, 1)
-    frame = cv.putText(img=frame,
-                       text="FPS: " + str(fps),
-                       org=(7, 30),
-                       fontFace=cv.FONT_HERSHEY_SIMPLEX,
-                       fontScale=1, 
-                       color=(100, 255, 0),
-                       thickness=2,
-                       )
+    frame = cv.flip(frame, 1) #flip image horizontally
 
+    overlay(frame) #adding overlay to video
+    
     cv.imshow(WINDOW_NAME, frame) #display framerate and flip camera horzontally
 
     #program waits 1ms each between frames checks for keypress of escape
