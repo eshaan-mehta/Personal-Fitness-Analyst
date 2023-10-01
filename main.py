b25d53e7801239b45c5573040c532a80d1db27f3
eshaan-mehta <eshaanm.mehta@gmail.com>
2023-10-01 14:21:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from setup import *
 
-MIN_CONFIDENCE = 0.5
 OVERLAY_HEIGHT = 100
+FRAME_INTERVAL = 5
+
+MIN_CONFIDENCE = 0.5
 UP_THRESHOLD = 150
 DOWN_THRESHOLD = 100
 
-status_list = {0:"OFF", 1:"RESET", 2:"ACTIVE"}
+STATUS_LIST = {0:"OFF", 1:"RESET", 2:"ACTIVE"}
+STATUS_COLOR = {0:(0,0,0), 1:(0,0,160), 2:(0,190,0)}
 status = 0
 
 front_knee = []
 back_knee = []
 back_side = []
 front_leg = ""
+
+#points for analytics
+frames = []
+depth_points = []
+back_points = []
+balance_points = []
+frame_count = 0
 
 def reset() -> None:
     global front_knee_angle, back_knee_angle, hip_angle
@@ -32,8 +42,8 @@ def reset() -> None:
 
 def find_angle(start: list, middle: list, end: list) -> float:
     #calculate angle and convert to degrees
-    angle = np.arctan2(start[1] - middle[1], start[0] - middle[0]) - np.arctan2(end[1] - middle[1], end[0] - middle[0])
-    angle = np.abs(angle * 180/np.pi)
+    angle = arctan2(start[1] - middle[1], start[0] - middle[0]) - arctan2(end[1] - middle[1], end[0] - middle[0])
+    angle = abs(angle * 180/pi)
 
     #ensures angle between 0-180 degrees
     return (360 - angle) if angle > 180 else angle
@@ -50,18 +60,16 @@ def overlay(frame: cv.Mat) -> cv.Mat:
     #frame shape = (height, width, depth)
     #color is in BGR
     
-    status_color = {0:(0,0,0), 1:(0,0,160), 2:(0,190,0)}
-
     #status background
     frame = cv.rectangle(img=frame,
                          pt1=(frame.shape[1]//4*3, 0),
                          pt2=(frame.shape[1], frame.shape[0]//10),
-                         color=status_color[status],
+                         color=STATUS_COLOR[status],
                          thickness=cv.FILLED
                          )
     
     frame = cv.putText(img=frame,
-                       text=status_list[status],
+                       text=STATUS_LIST[status],
                        org=(frame.shape[1]//4*3 + 25, 50),
                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
                        fontScale=2.5,
@@ -136,11 +144,20 @@ def overlay(frame: cv.Mat) -> cv.Mat:
     #front leg
     frame = cv.putText(img=frame,
                        text=f"Front leg: {front_leg}",
-                       org=(frame.shape[1]//3*2, frame.shape[0] - OVERLAY_HEIGHT + 65),
+                       org=(frame.shape[1]//3, frame.shape[0] - OVERLAY_HEIGHT//2),
                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
                        fontScale=1.2,
                        color=(255,255,255),
                        thickness=2)
+
+    #balance
+    frame = cv.putText(img=frame,
+                       text=f"Balance: {balance}",
+                       org=(frame.shape[1]//3*2, frame.shape[0] - OVERLAY_HEIGHT + 65),
+                       fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                       fontScale=1.2,
+                       color=(255,255,255),
+                       thickness=2) 
     
     """
     if status == 1:
@@ -172,14 +189,18 @@ reset()
 while capture.isOpened():
     success, frame = capture.read() #extracts single frame from video, success if receiving video
 
-    if success:
+    if not success:
+        break
+    else:
         results = model(source=frame, verbose=False, conf=MIN_CONFIDENCE)
+        
 
         for person in results:
             #index 0 to go down one dimension within tensor
-            #only care about hip, knees, ankle keypoints
-            keypoints = person.keypoints.xy[0].tolist() 
+            #bring keypoints back to cpu and convert to list
+            keypoints = person.keypoints.xy[0].cpu().tolist() 
 
+            #use try, except block to prevent crashes if error arises for a frame
             try:  
                 #define relevent front, back keypoints list depending on direction of person and knee in front
                 if keypoints[1][0] < keypoints[3][0]: #user facing camera left
@@ -194,7 +215,6 @@ while capture.isOpened():
                         back_knee = [keypoints[11], keypoints[13], keypoints[15]] #left
                         back_side = [keypoints[6], keypoints[12], keypoints[13]] #left
                         front_leg = "Right"
-
                 if keypoints[1][0] >= keypoints[3][0]: #user facing camera right
                     if keypoints[15][0] > keypoints[16][0]: #left leg forward
                         front_knee = [keypoints[11], keypoints[13], keypoints[15]] #left
@@ -226,34 +246,31 @@ while capture.isOpened():
                         is_up = False
 
                 #set status to reset if elbows above shoulders
-                #note: y is flipped
+                #note: y is increase as you go down the screen
                 if keypoints[7][1] < keypoints[5][1] and keypoints[8][1] < keypoints[6][1]:
                     status = 1
 
-                    #reset stats
+                    #reset stats if not already reset
                     if front_knee_angle > 0:
                         reset()
-                    
                 elif status != 0: #set to active status when not in off status
                     status = 2
                 
             except:
-                pass
+                print(Exception)
             
-            annotated_frame = person.plot()#adding person bounding box to frame
+            annotated_frame = person.plot() #adding person bounding box to frame
 
-        #annotated_frame = cv.flip(annotated_frame, 1) #flip image horizontally
-        annotated_frame = overlay(annotated_frame)
+
+        annotated_frame = overlay(annotated_frame) #adding all overlay
         
         #cv.resizeWindow(WINDOW_NAME, 900, 700)
-        cv.imshow(WINDOW_NAME, annotated_frame) #display framerate and flip camera horzontally
+        cv.imshow(WINDOW_NAME, annotated_frame) #display frame
 
-        #program waits 1ms each between frames checks for keypress of escape
-        # 27 is ASCII of escape key
+        #program waits 1ms each between frames checks for keypress of 'q'
         if (cv.waitKey(1) & 0xFF) == ord('q'):
             break
-    else:
-        break
+    
 
 capture.release()
 cv.destroyAllWindows()
