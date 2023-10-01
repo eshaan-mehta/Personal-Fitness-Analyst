5fbe162b0bf26114730735d82d1a7b8cf94596a5
Darius Rudaitis <dmruds123@gmail.com>
2023-10-01 19:34:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ OVERLAY_HEIGHT = 100
 FRAME_INTERVAL = 5
 
 MIN_CONFIDENCE = 0.5
+ANIM_FRAMES = 12
+
 UP_THRESHOLD = 150
 DOWN_THRESHOLD = 100
-BALANCE_THRESHOLD = 20
+STRAIGHTNESS_THRESHOLD = 65
+FRONT_FOOT_THRESHOLD = 87
 
-STATUS_LIST = {0:"OFF", 1:"RESET", 2:"ACTIVE"}
-STATUS_COLOR = {0:(0,0,0), 1:(0,0,160), 2:(0,190,0)}
+
+STATUS_LIST = {0:"OFF", 1:"RESET", 2:"ACTIVE", 3:"END"}
+STATUS_COLOR = {0:(0,0,0), 1:(0,0,160), 2:(0,190,0), 3:(153,51,102)}
 status = 0
 
 front_knee = []
@@ -17,26 +21,33 @@ back_knee = []
 back_side = []
 front_leg = ""
 
-#points for analytics
-frames = []
-depth_points = []
-back_points = []
-balance_points = []
-frame_count = 0
+
+
+
 
 def reset() -> None:
     global front_knee_angle, back_knee_angle, hip_angle
-    global back_straightness, knee_to_ankle_x, depth, balance, cur_shoulders, prev_shoulders, num_reps, is_up
+    global back_straightness, knee_to_ankle_x, depth, num_reps, is_up
+    global straightness_warnings, foot_warnings, frames, depth_points, rep_points, frame_count
+    global anim_count_back, anim_count_knee, graph
 
     front_knee_angle = 0
     back_knee_angle = 0
     hip_angle = 0
 
     back_straightness = 0 #percentage
-    balance = 0
-    cur_shoulders = 0 
-    prev_shoulders = 0
+    
+    #points for analytics
+    straightness_warnings = 0
+    foot_warnings = 0
+    frames = []
+    depth_points = []
+    rep_points = []
+    frame_count = 0
 
+    anim_count_back = 0
+    anim_count_knee = 0
+    graph = False
 
     num_reps = 0
     is_up = True
@@ -58,6 +69,14 @@ def deviation(target: float, range: float, angle: float) -> float:
 
     return 0 if (target - angle > range) else round(percent*100, 2)
 
+def create_graph():
+    plt.plot(frames, depth_points)
+    plt.xticks(rep_points, (f"Rep {i+1}" for i in range(len(rep_points))))
+    plt.ylabel("Height")
+    plt.title("Lunge Depth and Rep Count")
+    plt.savefig("graphs/depth.png")
+    plt.show()
+
 def overlay(frame: cv.Mat) -> cv.Mat:
     #frame shape = (height, width, depth)
     #color is in BGR
@@ -69,7 +88,7 @@ def overlay(frame: cv.Mat) -> cv.Mat:
                          color=STATUS_COLOR[status],
                          thickness=cv.FILLED
                          )
-    
+    #status text
     frame = cv.putText(img=frame,
                        text=STATUS_LIST[status],
                        org=(frame.shape[1]//4*3 + 25, 50),
@@ -97,14 +116,15 @@ def overlay(frame: cv.Mat) -> cv.Mat:
                        color=(255,255,255),
                        thickness=2)
     
-    #up/down status
-    frame = cv.putText(img=frame,
-                       text= "up" if is_up else "down",
-                       org=(300, 50),
-                       fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
-                       fontScale= 1.7,
-                       color=(255,255,255),
-                       thickness=1)
+    if status != 3:
+        #up/down status
+        frame = cv.putText(img=frame,
+                        text= "up" if is_up else "down",
+                        org=(300, 50),
+                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                        fontScale= 1.7,
+                        color=(255,255,255),
+                        thickness=1)
 
 #-----------------------------------------------------------------------------
 
@@ -115,75 +135,93 @@ def overlay(frame: cv.Mat) -> cv.Mat:
                          color=(0,0,0),
                          thickness=cv.FILLED
                          )
-    
-    #front knee angle text
-    frame = cv.putText(img=frame,
-                       text=f"Front Knee Angle: {round(front_knee_angle, 2)}",
-                       org=(25, frame.shape[0] - OVERLAY_HEIGHT + 25),
-                       fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
-                       fontScale=1.2,
-                       color=(255,255,255),
-                       thickness=2)
-    
-    #back knee angle text
-    frame = cv.putText(img=frame,
-                       text=f"Back Knee Angle: {round(back_knee_angle, 2)}",
-                       org=(25, frame.shape[0] - OVERLAY_HEIGHT + 65),
-                       fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
-                       fontScale=1.2,
-                       color=(255,255,255),
-                       thickness=2)
-    
-    #back straightness
-    frame = cv.putText(img=frame,
-                       text=f"Back Straighness: {back_straightness}%",
-                       org=(frame.shape[1]//3*2, frame.shape[0] - OVERLAY_HEIGHT + 25),
-                       fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
-                       fontScale=1.2,
-                       color=(255,255,255),
-                       thickness=2)
-    
-    #front leg
-    frame = cv.putText(img=frame,
-                       text=f"Front leg: {front_leg}",
-                       org=(frame.shape[1]//3, frame.shape[0] - OVERLAY_HEIGHT//2),
-                       fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
-                       fontScale=1.2,
-                       color=(255,255,255),
-                       thickness=2)
+    if status == 3:
+        frame = cv.putText(img=frame,
+                        text=f"Total Back Warnings: {straightness_warnings}",
+                        org=(25, frame.shape[0] - OVERLAY_HEIGHT + 35),
+                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                        fontScale=1.2,
+                        color=(255,255,255),
+                        thickness=2)
+        
+        frame = cv.putText(img=frame,
+                        text=f"Total Foot Warnings: {foot_warnings}",
+                        org=(frame.shape[1]//2 + 25, frame.shape[0] - OVERLAY_HEIGHT + 35),
+                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                        fontScale=1.2,
+                        color=(255,255,255),
+                        thickness=2)
+        
+        
 
-    #balance
-    frame = cv.putText(img=frame,
-                       text=f"Balance: {balance}",
-                       org=(frame.shape[1]//3*2, frame.shape[0] - OVERLAY_HEIGHT + 65),
-                       fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
-                       fontScale=1.2,
-                       color=(255,255,255),
-                       thickness=2) 
+        
+    else:
+        #front knee angle text
+        frame = cv.putText(img=frame,
+                        text=f"Front Knee Angle: {round(front_knee_angle, 2)}",
+                        org=(25, frame.shape[0] - OVERLAY_HEIGHT + 25),
+                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                        fontScale=1.2,
+                        color=(255,255,255),
+                        thickness=2)
+        
+        #back knee angle text
+        frame = cv.putText(img=frame,
+                        text=f"Back Knee Angle: {round(back_knee_angle, 2)}",
+                        org=(25, frame.shape[0] - OVERLAY_HEIGHT + 65),
+                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                        fontScale=1.2,
+                        color=(255,255,255),
+                        thickness=2)
+        
+        #back straightness
+        frame = cv.putText(img=frame,
+                        text=f"Back Straighness: {back_straightness}%",
+                        org=(frame.shape[1]//3*2, frame.shape[0] - OVERLAY_HEIGHT + 25),
+                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                        fontScale=1.2,
+                        color=(255,255,255),
+                        thickness=2)
+        
+        #front leg
+        frame = cv.putText(img=frame,
+                        text=f"Front leg: {front_leg}",
+                        org=(frame.shape[1]//3, frame.shape[0] - OVERLAY_HEIGHT//2),
+                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                        fontScale=1.2,
+                        color=(255,255,255),
+                        thickness=2)
+
+        #balance
+        frame = cv.putText(img=frame,
+                        text=f"Hip Angle: {round(hip_angle, 2)}",
+                        org=(frame.shape[1]//3*2, frame.shape[0] - OVERLAY_HEIGHT + 65),
+                        fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                        fontScale=1.2,
+                        color=(255,255,255),
+                        thickness=2)
     
-    """
-    if status == 1:
-        frame = cv.circle(img=frame, 
-                          center=(int(keypoints[5][0]), int(keypoints[5][1])),
-                          radius=15,
-                          color=status_color[status],
-                          thickness=cv.FILLED)
-        frame = cv.circle(img=frame, 
-                          center=(int(keypoints[6][0]), int(keypoints[6][1])),
-                          radius=15,
-                          color=status_color[status],
-                          thickness=cv.FILLED)
-        frame = cv.circle(img=frame, 
-                          center=(int(keypoints[7][0]), int(keypoints[7][1])),
-                          radius=15,
-                          color=status_color[status],
-                          thickness=cv.FILLED)
-        frame = cv.circle(img=frame, 
-                          center=(int(keypoints[8][0]), int(keypoints[8][1])),
-                          radius=15,
-                          color=status_color[status],
-                          thickness=cv.FILLED)
-    """
+    return frame
+    
+def add_warning(frame: cv.Mat, warning_type: int) -> cv.Mat:
+    #straightness warning
+    if warning_type == 1:    
+        frame = cv.putText(img=frame,
+                            text="STRAIGHTEN BACK",
+                            org=(int(back_side[0][0]), int(back_side[0][1])),
+                            fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                            fontScale=2.8,
+                            color=(15,15,255),
+                            thickness=3)
+    else: #front knee warning
+        frame = cv.putText(img=frame,
+                            text="MOVE FRONT KNEE BACK",
+                            org=(int(front_knee[1][0]), int(front_knee[1][1])),
+                            fontFace=cv.FONT_HERSHEY_COMPLEX_SMALL,
+                            fontScale=2.8,
+                            color=(0, 216, 255),
+                            thickness=3)
+        
     return frame
 
 
@@ -195,8 +233,7 @@ while capture.isOpened():
         break
     else:
         results = model(source=frame, verbose=False, conf=MIN_CONFIDENCE)
-        
-
+       
         for person in results:
             #index 0 to go down one dimension within tensor
             #bring keypoints back to cpu and convert to list
@@ -204,7 +241,7 @@ while capture.isOpened():
 
             #use try, except block to prevent crashes if error arises for a frame
             try:  
-                #define relevent front, back keypoints list depending on direction of person and knee in front
+#-------------- FRONT/BACK KEYPOINT DETECTION ----------------------
                 if keypoints[1][0] < keypoints[3][0]: #user facing camera left
                     if keypoints[15][0] < keypoints[16][0]: #left leg forward
                         front_knee = [keypoints[11], keypoints[13], keypoints[15]] #left
@@ -229,46 +266,89 @@ while capture.isOpened():
                         back_knee = [keypoints[11], keypoints[13], keypoints[15]] #left
                         back_side = [keypoints[6], keypoints[12], keypoints[13]] #left
                         front_leg = "Right"
-
-                prev_shoulders = abs(keypoints[5][1] - keypoints[6][1])
-
-                #tracking user status if in active status
+                
+                
+                #----------- ACTIVE MODE -------------------
                 if status == 2:
+                    #----------- STAT CALCULATIONS -----------
                     #calculating angles at knees, hip
                     front_knee_angle = find_angle(front_knee[0], front_knee[1], front_knee[2])
                     back_knee_angle = find_angle(back_knee[0], back_knee[1], back_knee[2])
                     hip_angle = find_angle(back_side[0], back_side[1], back_side[2])
 
-                    cur_shoulders = abs(keypoints[5][1] - keypoints[6][1])
-
-                    balance = deviation(prev_shoulders, BALANCE_THRESHOLD, cur_shoulders)
-                    
                     #calculate back straighness using hip angle
-                    back_straightness = deviation(157, 80, hip_angle)
+                    back_straightness = deviation(142 if is_up else 157, 60, hip_angle)
 
-                    #counting repts
+                    #----------- REP COUNTING --------------------------
                     if front_knee_angle > UP_THRESHOLD and not is_up:
                         num_reps += 1
                         is_up = True
+
+                        rep_points.append(frame_count)
                     elif is_up and (front_knee_angle < DOWN_THRESHOLD):
                         is_up = False
 
-                #set status to reset if elbows above shoulders
-                #note: y is increase as you go down the screen
-                if keypoints[7][1] < keypoints[5][1] and keypoints[8][1] < keypoints[6][1]:
-                    status = 1
+                    #----------- WARNING DETECTION AND OVERLAY ----------  
+                    #detect poor back straightness
+                    if back_straightness < STRAIGHTNESS_THRESHOLD and not is_up and anim_count_back == 0:
+                        straightness_warnings += 1
+                        anim_count_back = 1
 
+                    #detect front knee too far forward
+                    if front_knee_angle < FRONT_FOOT_THRESHOLD and not is_up and anim_count_knee == 0:
+                        foot_warnings += 1
+                        anim_count_knee = 1
+                        
+                
+
+                    # ----------- FRAME COUNTING ---------------------------
+                    frame_count += 1
+                    if frame_count % 3 == 0:
+                        frames.append(frame_count)
+                        depth_points.append(frame.shape[0] - front_knee[0][1])
+                
+                #----------- END MODE -------------------
+                if status == 3:
+                    if not graph:
+                        create_graph()
+                        graph = True
+
+                #-----------  STATUS UPDATE --------------------------------
+
+                #set status to reset if left wrist above shoulder
+                #note: y increases as you go down the screen
+                if keypoints[9][1] < keypoints[5][1]:
+                    status = 1
                     #reset stats if not already reset
                     if front_knee_angle > 0:
                         reset()
-                elif status != 0: #set to active status when not in off status
-                    status = 2
-                
-            except:
-                print(Exception)
-            
-            annotated_frame = person.plot() #adding person bounding box to frame
 
+                elif keypoints[10][1] < keypoints[6][1]: #set status to quit if right wrist above shoulder
+                    status = 3
+                else: 
+                    if status == 1: #set to active status after reset
+                        status = 2
+
+
+            except:
+                pass
+            
+            annotated_frame = person.plot() #adding person bounding box to frame 
+
+        #---------- DISPLAY WARNING ------------
+        #display back warning
+        if anim_count_back > 0  and anim_count_back < ANIM_FRAMES:
+            annotated_frame = add_warning(annotated_frame, 1)
+            anim_count_back += 1
+        elif anim_count_back >= ANIM_FRAMES:
+            anim_count_back = 0
+
+        #display knee warning
+        if anim_count_knee > 0  and anim_count_knee < ANIM_FRAMES:
+            annotated_frame = add_warning(annotated_frame, 2)
+            anim_count_knee += 1
+        elif anim_count_knee >= ANIM_FRAMES:
+            anim_count_knee = 0
 
         annotated_frame = overlay(annotated_frame) #adding all overlay
         
